0ef9c470511235261620822072a333e8c2300f19
wavem <fenix24@naver.com>
2021-01-15 15:26:19 +0900

    [New] iocomp plot test
    - iocomp plot 을 form 에 띄워봤다.
    - 라이센스 경고 없이 잘 실행된다.
    - 이제 이를 빌더가 안깔린 PC 에서 실행시켜봤다.
    - iocomp plot 관련 bpl 이 없다고 하면서 안되더라.
    - 따라서, 빌더가 없는 PC 에서 실행 되도록 프로젝트 옵션을 바꾸어보았다.
    - iocomp plot 관련한 bpl 등은 따로 추가하지 않았다.
    - 잘 된다. 이런식으로 쓰면 되겟다.

--- XE7_iocomp_Test/Unit1.cpp
+++ XE7_iocomp_Test/Unit1.cpp
@@ -6,6 +6,11 @@
 #include "Unit1.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+#pragma link "iComponent"
+#pragma link "iCustomComponent"
+#pragma link "iPlot"
+#pragma link "iPlotComponent"
+#pragma link "iVCLComponent"
 #pragma resource "*.dfm"
 TForm1 *Form1;
 //---------------------------------------------------------------------------
--- XE7_iocomp_Test/Unit1.h
+++ XE7_iocomp_Test/Unit1.h
@@ -7,10 +7,16 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include "iComponent.hpp"
+#include "iCustomComponent.hpp"
+#include "iPlot.hpp"
+#include "iPlotComponent.hpp"
+#include "iVCLComponent.hpp"
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+	TiPlot *iPlot1;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
